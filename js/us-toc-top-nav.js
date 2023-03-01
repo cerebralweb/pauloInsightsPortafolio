0a80c9c5b868d2e08953c93bea6e8c796362573e
cerebralweb <paulocleon@gmail.com>
2023-03-01 09:36:45 -0500

    article

--- js/us-toc-top-nav.js
+++ js/us-toc-top-nav.js
@@ -5,13 +5,14 @@ const top_nav = `
 </a>
 <!--<<<<<<<<<<< <a href="https://twitter.com/clpalacios_in" target="_blank" class="buttonUnit_topNav class1">Twitter</a> >>>>>>>>>>>-->
 <a href="#us-newsletter" class="buttonUnit_topNav class1">Subscribe</a>
-<a href="#free" class="buttonUnit_topNav class1">Free Stories</a>
+<a href="#free" class="buttonUnit_topNav class1">Free Story</a>
 <!--<<<<<<<<<<< <a href="/us-podcast.html" target="_blank" class="buttonUnit_topNav class1">Podcast</a>>>>>>>>>>>>-->
 <a href="/us-infographics.html" target="_blank" class="buttonUnit_topNav class1">Diary of a Z Girl</a>
 
 <a href="https://www.instagram.com/clpalacios.in/" target="_blank" class="buttonUnit_topNav class1">Instagram</a>
 <a href="https://www.instagram.com/clpalacios.in/" target="_blank" class="buttonUnit_topNav class1">TikTok</a>
 <a href="#tip" class="buttonUnit_topNav class1">About</a>
+<a href="https://www.clpalacios.in/" class="buttonUnit_topNav class1">Home</a>
 
 `;
 
