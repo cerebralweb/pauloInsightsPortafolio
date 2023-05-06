e8affcf355746fc5701219903d119f0e94751d80
cerebralweb <paulocleon@gmail.com>
2023-05-06 09:12:49 -0500

    social

--- js/us-toc-top-nav.js
+++ js/us-toc-top-nav.js
@@ -3,14 +3,13 @@ const top_nav = `
 <!--<<<<<<<<<<< <a href="/es-toc.html" class="buttonUnit_topNav class1">ES</a> >>>>>>>>>>>-->
 
 </a>
-<!--<<<<<<<<<<< <a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Twitter</a> >>>>>>>>>>>-->
+<!--<<<<<<<<<<< <a href="https://twitter.com/clpalacios_in" target="_blank" class="buttonUnit_topNav class1">Twitter</a> >>>>>>>>>>>-->
 <a href="#us-newsletter" class="buttonUnit_topNav class1">Subscribe</a>
 <a href="#free" class="buttonUnit_topNav class1">Free Story</a>
 <!--<<<<<<<<<<< <a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Podcast</a>>>>>>>>>>>>-->
 <a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Diary of Z </a>
 
-<a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Instagram</a>
-<a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">TikTok</a>
+<a href="https://www.instagram.com/clpalaciosbooks/" target="_blank" class="buttonUnit_topNav class1">Instagram</a>
 <a href="#tip" class="buttonUnit_topNav class1">About</a>
 <a href="/index.html" class="buttonUnit_topNav class1">Home</a>
 
