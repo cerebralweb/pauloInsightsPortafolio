6c6640bebeffed1630ab601d8658ac607a246869
cerebralweb <paulocleon@gmail.com>
2023-09-01 09:17:09 -0500

    form x

--- js/us-newsletter.js
+++ js/us-newsletter.js
@@ -4,23 +4,35 @@ const newsletter_es = `
             
 <p>Dear reader, subscribe and receive for free a collection of short stories. Subscribers can take part in
 marketing research projects (and get prizes), and being beta readers </p>
-
-<form name="contact" netlify>
+<form 
+    name="contact CL Palacios" 
+    action="/us-thanks-for-your-submission"  
+    
+    netlify>
+  
+  
+  
   <p>
-    <label>Name <input type="text" name="name" /></label>
+  <label for="email">Your email:
+    <input 
+    name="email"
+    type="email"
+    required
+    title="It should be a valid email :)"
+    pattern="[a-zA-Z0-9_.]{5,31}@(([a-zA-Z0-9]{2,6}\.)([a-zA-Z0-9]{2,4})){1}(\.[a-zA-Z0-9]{2,3})?"
+    >
+    
+  </label>
   </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
+ 
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
-
 `;
 document.getElementById('us-newsletter').insertAdjacentHTML('afterbegin', newsletter_es);
 //problem ? in the regex: an escaped dot (just erase the regex)
-//action="/us-thanks-for-your-submission"   
+//action="/us-infographics-404.html"  
 //    data-netlify="true"
 //method="POST"
 
