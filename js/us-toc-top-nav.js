5f432910fa13d030802364252e78a8f62b4c915a
César L. Palacios <paulocleon@gmail.com>
2024-09-30 14:24:08 -0600

    new web consultant

--- js/us-toc-top-nav.js
+++ js/us-toc-top-nav.js
@@ -5,9 +5,9 @@ const top_nav = `
 </a>
 <!--<<<<<<<<<<< <a href="https://twitter.com/clpalacios_in" target="_blank" class="buttonUnit_topNav class1">Twitter</a> >>>>>>>>>>>-->
 <a href="#us-newsletter" class="buttonUnit_topNav class1">Subscribe</a>
-<a href="#free" class="buttonUnit_topNav class1">Free Story</a>
-<!--<<<<<<<<<<< <a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Podcast</a>>>>>>>>>>>>-->
-<a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Diary of Z </a>
+<a href="#free" class="buttonUnit_topNav class1">Free Cases</a>
+<!--<<<<<<<<<<< <a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Podcast(soon)</a>>>>>>>>>>>>-->
+<a href="/us-infographics-404.html" target="_blank" class="buttonUnit_topNav class1">Portafolio </a>
 
 <a href="https://www.instagram.com/clpalaciosbooks/" target="_blank" class="buttonUnit_topNav class1">Instagram</a>
 <a href="#tip" class="buttonUnit_topNav class1">About</a>
