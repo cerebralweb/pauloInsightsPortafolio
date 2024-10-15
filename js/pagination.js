5b4c5e2cb620260baa249dda72f49361b67b7701
César L. Palacios <paulocleon@gmail.com>
2024-10-15 17:35:38 -0600

    new web consultant

--- js/pagination.js
+++ js/pagination.js
@@ -16,7 +16,7 @@
  */
     const articlesDataBase = [
         {
-            articleTitle: "a/Babies Safety Products",
+            articleTitle: "Babies Safety Products",
             articleDescription: "How to sell to a generation of parents concerned about safety and success? In this study we analyze possible gaps in baby brands",
             articleHref: "/a/babies-safety-products.html"
         },
@@ -29,7 +29,73 @@
             articleTitle: "La máquina de insights de Airbnb",
             articleDescription: "Airbnb tiene una fortuna en datos. Vienen de los 200 millones de personas que han reservado viajes",
             articleHref: "/a/arbnb-y-sus-estrategias-para-obtener-insights.html"
-        },           
+        },  
+        {
+            articleTitle: "Q&A Session About CL Palacios",
+            articleDescription: "This are some curious facts about the writer behind Diary of a Z Girl!!.",
+            articleHref: "/a/cl-palacios-author-interview.html"
+        },
+        {
+            articleTitle: "Q&A Session About The Diary of a Z Girl",
+            articleDescription: "I wrote this interview to explain what is Diary of Z Girl.",
+            articleHref: "/a/diary-of-a-z-girl-interview.html"
+        },
+        {
+            articleTitle: "Are Science Fiction and Fantasy Different?",
+            articleDescription: "Why is science fiction so hard to define? It tends to pay more attention to the harmful sides of civilization. The future is the safest laboratory to try out fiction ideas.",
+            articleHref: "/a/are-science-fiction-fantasy-different.html"
+        },
+        {
+            articleTitle: "Think Like A Dinosaur is a Happy Exception in High Concept Science Fiction",
+            articleDescription: "I explain you how Science Fiction and Fantasy are similar and Different, so you can decide which novel you'll write.",
+            articleHref: "/a/think-like-a-dinosaur-james-Patrick-kelly-scifi.html"
+        },
+        {
+            articleTitle: "The Origins Of Matrix, Chrome, And A Bunch Of Hackers Fighting For Money And Love",
+            articleDescription: "I tell the origins of the concept of Matrix and comment on a SciFi classic.",
+            articleHref: "/a/origins-of-matrix-william-gibson-burning-chrome.html"
+        },
+        {
+            articleTitle: "Best Viewpoints For Fiction And How To Build Them",
+            articleDescription: "I talk about what viewpoint is, how to use it in writing, and how to shifting viewpoints",
+            articleHref: "/a/best-viewpoints-for-fiction-and-how-to-build-them.html"
+        },
+        {
+            articleTitle: "Dialogue And Pace For Happy Audiences",
+            articleDescription: "Learn how scenes are combinations of Dramatic action (units of action and reaction) and Dialogue. Also: pointless conversation destroys a novel; and  the aftermath of good dialogue is conflict.",
+            articleHref: "/a/dialogue-and-pace-for-happy-audiences.html"
+        },
+        {
+            articleTitle: "Make The Reader Want To Know What Happens Next",
+            articleDescription: "Saavy writers know that the scenes are a line of domino pieces. But how do you align them correctly? ",
+            articleHref: "/a/how-to-create-a-page-turner-novel.html"
+        },
+        {
+            articleTitle: "Setting A Good Story And A Strong Bond Between The Reader And The Hero",
+            articleDescription: "I explain you how to write a novel that provides an emotional elaboration and a rich plot.",
+            articleHref: "/a/how-to-make-a-page-turner-reader-and-hero.html"
+        },
+        {
+            articleTitle: "How To Write A Good Scene",
+            articleDescription: "I tell you how to use conflict in scenes, which are the types of scenes, and how to set The Big Scene",
+            articleHref: "/a/how-to-write-a-good-scene.html"
+        },
+        {
+            articleTitle: "An Empty Text 1",
+            articleDescription: "This is a unique form of invisible fiction 1",
+            articleHref: "/us-infographics-404.html"
+        },
+        {
+            articleTitle: "An Empty Text 2",
+            articleDescription: "This is a unique form of invisible fiction 2",
+            articleHref: "/us-infographics-404.html"
+        },
+        {
+            articleTitle: "An Empty Text 3",
+            articleDescription: "This is a unique form of invisible fiction 3",
+            articleHref: "/index.html"
+        }
+           
     ];
     console.log(articlesDataBase.length);
 
