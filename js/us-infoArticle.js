1eede3260e30f266c2a4ef8ed82e6f318a580683
Paulo C. Leon <paulocleon@gmail.com>
2026-05-16 13:08:54 -0600

    new BI blog 2026

--- js/us-infoArticle.js
+++ js/us-infoArticle.js
@@ -3,7 +3,7 @@ const info_del_articulo = `<h4>Content</h4>
 <p><a class="class2" href="#business-problem">Business Problem</a></p>
 <p><a class="class2" href="#math-answer">Math answer</a></p>
 <p><a class="class2" href="#python-version">Python version</a></p>
-<p><a class="class2" href="#claude-skill">Claude Skills version</a></p>
+<p><a class="class2" href="#skill-file">Claude Skills version</a></p>
 `
 ;
 
